f28ba7624e9b737588923257c7141c4a2d3971ec
Enrico Simonetti <enrico.simonetti@gmail.com>
2017-02-23 13:18:28 +1100

    Improvements on retrieving blowfish key only once and logging

--- src/custom/include/encryption/EnvCrypt.php
+++ src/custom/include/encryption/EnvCrypt.php
@@ -10,13 +10,13 @@ class EnvCrypt
         $result = blowfishGetKey('custom_encrypt');
 
         if(empty($sugar_config['dbconfig']['filesystem_encryption_only'])) {
-            if (false !== getenv('sugar_blowfish_key')) {
-                $key = getenv('sugar_blowfish_key');
+            $key = getenv('sugar_blowfish_key');
+            if ($key !== false) {
                 if (strlen($key) > 56 || strlen($key) < 4) {
                     $GLOBALS['log']->security("Retrieved encryption key from ENV variable 'sugar_blowfish_key'. It's  " . strlen($key)
-                            . " chars long but should be more than 4 chars but less than 56 chars long (32 to 448 bits).");
+                            . " chars long but should be more than 4 chars but less than 56 chars long (32 to 448 bits). Will use the file system based one.");
                 } else {
-                    $result = getenv('sugar_blowfish_key');
+                    $result = $key;
                 }
             } else {
                 $GLOBALS['log']->security("Could not retrieve encryption key from ENV variable 'sugar_blowfish_key'. Will use the file system based one. To suppress this security message and leverage only file system based blowfish encryption set on config_override.php \$sugar_config['dbconfig']['filesystem_encryption_only'] = true;");
